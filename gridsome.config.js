fd82059c25e1653a80cd126c7848e3c9ac57626a
zkriszti <zavecz.krisztina@gmail.com>
2019-12-09 21:13:34 +0100

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,29 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Noemi Mate - Digital Marketing for Small Business',
+
+  port: 8900,
+
+  transformers: {
+    remark: {
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "blog/**/*.md",
+        typeName: "Post"
+      }
+    }
+  ]
+}
