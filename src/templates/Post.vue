f8d8cc6db2afeea58ee5ff0ed7a64509f645bd3f
zkriszti <zavecz.krisztina@gmail.com>
2019-12-10 21:54:29 +0100

    Post template

---
+++ src/templates/Post.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <h3>{{ $page.post.title }}</h3>
+    <div class="date-meta-container">
+      <p>{{ $page.post.date }}</p>
+    </div>
+    <div v-html="$page.post.content" />
+  </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  post: post (path: $path) {
+    title
+    content
+    date
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    };
+  }
+};
+</script>
+
+<style lang="stylus">
+h3
+  color: #777
+  font-weight: normal
+  display: flex
+  align-items: center
+
+</style>
+
