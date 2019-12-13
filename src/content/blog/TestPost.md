ef846f466eef937b775d4a6377787f274534d482
zkriszti <zavecz.krisztina@gmail.com>
2019-12-13 21:57:30 +0100

    no message

---
+++ src/content/blog/TestPost.md
@@ -0,0 +1,7 @@
+---
+title: TestPost
+date: 13/12/2019
+slug: testpost
+
+---
+Eu eaque yet aliquid laboris or quia so molestiae. Anim fugit. Numquam aut nor cillum reprehenderit mollit velit nulla. Nostrud occaecat for unde. Doloremque omnis error. Laboris sequi. Corporis sint, so vitae ea. Ullam eu and adipisci ipsa or pariatur so eos.
---
+++ src/templates/BlogPost.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+    <h3>{{ $page.blogPost.title }}</h3>
+    <div class="date-meta-container">
+      Hey, it's the blog!
+      <p>{{ $page.blogPost.id }}</p>
+    </div>
+    <div v-html="$page.blogPost.content" />
+  </Layout>
+</template>
+
+<page-query>
+query blogPost ($id: ID!) {
+  blogPost (id: $id) {
+    id
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.blogPost.title
+    };
+  }
+};
+</script>
+
+<style>
+/* h3
+  color: #777
+  font-weight: normal
+  display: flex
+  align-items: center
+ */
+</style>
+
