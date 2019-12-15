b1aa04f02a2041ea4ab06789d0d14af62fe4e0dd
zkriszti <zavecz.krisztina@gmail.com>
2019-12-15 20:36:40 +0100

    add blog menu item

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,6 +8,7 @@
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
         <g-link class="nav__link" to="/contact/">Contact</g-link>
+        <g-link class="nav__link" to="/blog/">Blog</g-link>
       </nav>
     </header>
     <slot/>
