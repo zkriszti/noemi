11d9fc4bf1edd48dc215e92d3c322130a990867a
zkriszti <zavecz.krisztina@gmail.com>
2019-12-15 13:48:21 +0100

    add contact page and menu item

---
+++ src/pages/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <h1>Contact Noemi</h1>
+    <p>This is a contact form here.</p>
+    <form name="contact" method="POST" netlify netlify-honeypot="hpfield">
+      <div class="hpot">
+        <label>Leave this field empty: <input name="hpfield"></label>
+      </div>
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <button type="submit">Send</button>
+    </form>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contact Noemi'
+  }
+}
+</script>
+
+<style scoped>
+  .hpot {
+  display: none;
+}
+
+</style>
