18e537199ef4bf71d74ed2c9f053887b06f23d5c
zkriszti <zavecz.krisztina@gmail.com>
2019-12-19 11:18:30 +0100

    added SiteHeader as component

---
+++ src/layouts/SiteHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <strong>
+      <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+    </strong>
+    <nav class="nav">
+      <g-link class="nav__link" to="/">Home</g-link>
+      <a class="nav__link" href="#about">About</a>
+      <a class="nav__link" href="#contact">Contact</a>
+      <a class="nav__link" href="#blog">Blog</a>
+      <g-link class="nav__link" to="/blog/">Blog -- SA</g-link>
+      <g-link class="nav__link" to="/about/">About -- SA</g-link>
+    </nav>
+  </header>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
