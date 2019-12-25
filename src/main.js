75931462a813dace5a2c31310bedb9f52c6e4c5f
zkriszti <zavecz.krisztina@gmail.com>
2019-12-26 00:43:58 +0100

    added vue-mq to project / made hash-scroll work

--- src/main.js
+++ src/main.js
@@ -2,14 +2,38 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import VueMq from 'vue-mq'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
+  console.log(router.options.scrollBehavior)
+
+  router.options.scrollBehavior = (to, from, savedPosition) => {
+    if (to.hash) {
+      return {
+        selector: to.hash
+        // , offset: { x: 0, y: 10 }
+        // behavior: 'smooth'
+      }
+    }
+  }
+
+
+
   // Add a meta tag
   head.meta.push({
     name: 'description',
     content: 'Noemi Mate | Digital Marketing & SEO Consultant from Budapest, Hungary'
+  })
+
+  Vue.use(VueMq, {
+    breakpoints: {
+      xs: 449,
+      sm: 767,
+      md: 1023,
+      lg: Infinity,
+    }
   })
 }
