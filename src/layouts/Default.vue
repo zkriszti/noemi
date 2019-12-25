3c9b2d970b769383352fbedeb123b8d212eed345
zkriszti <zavecz.krisztina@gmail.com>
2019-12-26 00:44:23 +0100

    added siteFooter

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,23 +1,31 @@
 <template>
-  <div :class="isSingle ? 'layout' : ''">
+  <div :class="[isSingle ? 'layout' : '', screenClass]" >
     <SiteHeader v-if="isSingle" />
     <div class="content"><slot /></div>
+    <SiteFooter v-if="isSingle" />
   </div>
 </template>
 
 <script>
-
 import SiteHeader from '@/layouts/SiteHeader.vue'
+import SiteFooter from '@/layouts/SiteFooter.vue'
 
 export default {
   components: {
-    SiteHeader
+    SiteHeader,
+    SiteFooter
   },
 
   props: {
     isSingle: {
       type: Boolean,
       default: false
+    }
+  },
+
+  computed: {
+    screenClass () {
+      return `screen-${this.$mq}`
     }
   }
 }
@@ -36,10 +44,14 @@ body
   line-height: 1.5
 
   .layout
-    max-width: 760px
+    max-width: 1920px
     margin: 0 auto
+    display: flex
+    min-height: 100vh
+    flex-direction: column
 
   .content
     padding: 0 20px
+    flex-grow: 1
 
 </style>
---
+++ src/layouts/SiteFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer" ref="footer">
+    <!-- <g-link to="/">{{ $static.metadata.siteName }}</g-link> -->
+    <div class="footer-social">
+      <p>instagram (ikon)</p>
+      <p>pinterest (ikon)</p>
+      <p>linkedin (ikon)</p>
+      <p>kötelezők (impresszum, ászf / disclaimer, privacy policy, terms of conditions)</p>
+    </div>
+  </footer>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/colors.styl'
+
+footer
+  background: #eee
+  p:last-child
+    margin-bottom: 0
+
+</style>
