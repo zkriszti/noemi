e55224300acf270db5c4dd505f8d645af109f85d
zkriszti <zavecz.krisztina@gmail.com>
2019-12-26 14:35:24 +0100

    added smooth scroll

--- src/main.js
+++ src/main.js
@@ -8,25 +8,13 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
-  console.log(router.options.scrollBehavior)
-
-  router.options.scrollBehavior = (to, from, savedPosition) => {
-    if (to.hash) {
-      return {
-        selector: to.hash
-        // , offset: { x: 0, y: 10 }
-        // behavior: 'smooth'
-      }
-    }
-  }
-
-
-
   // Add a meta tag
   head.meta.push({
     name: 'description',
     content: 'Noemi Mate | Digital Marketing & SEO Consultant from Budapest, Hungary'
   })
+
+  head.htmlAttrs = { style: 'scroll-behavior: smooth' }
 
   Vue.use(VueMq, {
     breakpoints: {
